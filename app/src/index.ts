e236cfbb905002a38f7db7ce4d2e6d673c01ed9d
もっちゃん <s7056i3056@gmail.com>
2025-05-04 20:19:38 +0900

    feat: 各メソッドを追加

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,9 +1,25 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.get("/api/hello", (c) => {
+  return c.json({
+    ok: true,
+    message: "hello hono api!",
+  });
+});
+
+app.get("/posts/:id", (c) => {
+  const page = c.req.query("page");
+  const id = c.req.param("id");
+  c.header("X-Message", "Hi!");
+  return c.text(`You want to see ${page} of ${id}`);
+});
+app.post("/posts", (c) => c.text("Created!", 201));
+app.delete("/posts/:id", (c) => c.text(`${c.req.param("id")} is deleted!`));
+
+export default app;
